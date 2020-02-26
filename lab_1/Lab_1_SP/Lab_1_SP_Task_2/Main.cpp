c36d2912def11696f91d76a359a2009f53cc948a
Oleh Tymoshenko <o.tymoshenko@student.csn.khai.edu>
2020-02-26 22:22:56 +0200

    Modify task 2. Add report

--- lab_1/Lab_1_SP/Lab_1_SP_Task_2/Main.cpp
+++ lab_1/Lab_1_SP/Lab_1_SP_Task_2/Main.cpp
@@ -1,9 +1,12 @@
+#define  _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include "windows.h"
 #include <locale.h>
 #include <iostream>
 
+
 using namespace std;
+
 
 #define ANSI_KEY "-a"
 #define UNICODE_KEY "-u"
@@ -13,11 +16,12 @@ using namespace std;
 void ANSI_To_Unicode(LPCSTR);
 void Unicode_To_ANSI(LPCSTR);
 void ShowError();
-
+bool ConcatFileNameWithString(char* FileName, char* StringToConcat, char* resultString, int len_resultString);
 
 int main(int argc, char* argv[])
 {
     setlocale(LC_CTYPE, "rus");
+
     if (argc != 3)
     {
         fprintf(stderr, "Ошибка! Не переданы все аргументы дял программы\n");
@@ -29,7 +33,6 @@ int main(int argc, char* argv[])
     }
     else
     {
-        printf("%s", argv[1]);
         if (strcmp(argv[1], ANSI_KEY) == 0)
         {
             ANSI_To_Unicode(argv[2]);
@@ -43,7 +46,7 @@ int main(int argc, char* argv[])
             fprintf(stderr, "Ошибка ввода параметров\n");
         }
     }
-    fprintf(stdout, "Конвертация была успешно выполнена");
+    fprintf(stdout, " ***Конвертация была успешно выполнена *** ");
     return 0;
 }
 
@@ -57,9 +60,10 @@ void ANSI_To_Unicode(LPCSTR fileName)
     DWORD countReadedChars, countWritenChars; // counters of read Symbols(in ReadFIle) and writen Symbols(in WriteFile)  
     BOOL resultRdFile = true; 
 
-    char nameDestinationFile[256]; // name to file with recoded text
-    snprintf(nameDestinationFile, sizeof(nameDestinationFile), "%s%s", "Encoded_To_Unicode_", fileName);
-    printf("Перекодированый текст будет находится в \n %s\n", nameDestinationFile);
+	char additionalTextToFileName[] = "_encoded_To_Unicode";
+	char nameDestinationFile[FILENAME_MAX]; // name to file with recoded text
+	ConcatFileNameWithString((char*)fileName, additionalTextToFileName, nameDestinationFile, FILENAME_MAX);
+    printf(" *** Перекодированый текст будет находится в *** \n %s\n", nameDestinationFile);
     // OPEN FILES
     handleSourceFile = CreateFile(fileName, GENERIC_READ, FILE_SHARE_READ,
                                   NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
@@ -110,9 +114,11 @@ void Unicode_To_ANSI(LPCSTR fileName)
     DWORD countReadedBytes, countWritenBytes; // counters of read Symbols(in ReadFIle) and writen Symbols(in WriteFile)  
     BOOL resultRdFile = true;
 
-    char nameDestinationFile[256]; // name to file with recoded text
-    snprintf(nameDestinationFile, sizeof(nameDestinationFile), "%s%s", "Encoded_To_ASCI_", fileName);
-    printf("Перекодированый текст будет находится в \n %s\n", nameDestinationFile);
+	char additionalTextToFileName[] = "_encoded_To_ANSI";
+	char nameDestinationFile[FILENAME_MAX]; // name to file with recoded text
+
+	ConcatFileNameWithString((char*)fileName, additionalTextToFileName, nameDestinationFile, FILENAME_MAX);
+    printf(" *** Перекодированый текст будет находится в *** \n %s\n", nameDestinationFile);
     // OPEN FILES
     handleSourceFile = CreateFile(fileName, GENERIC_READ, FILE_SHARE_READ,
         NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
@@ -153,6 +159,27 @@ void Unicode_To_ANSI(LPCSTR fileName)
 
     CloseHandle(handleSourceFile);
     CloseHandle(handleDestinationFile);
+}
+
+// This function concat file name (file name with format: .txt, .xml etc) with string,
+// this string insert in end of file name, but before format (i.e. before last dot in file name string)
+bool ConcatFileNameWithString(char * FileName, char * StringToConcat, char * resultString, int len_resultString)
+{	
+	for (int i = 0; i < len_resultString; i++)
+	{
+		resultString[i] = '\0';
+	}
+	if ((strlen(FileName) + strlen(StringToConcat)) > FILENAME_MAX)
+	{
+		resultString = NULL;
+		return false;
+	}
+	char * refToLastDot = strrchr(FileName, '.');
+	int lastDotPositionInFileName = refToLastDot - FileName;
+	strncpy(resultString, FileName, lastDotPositionInFileName);
+	snprintf(resultString, FILENAME_MAX, "%s%s%s", resultString, StringToConcat, refToLastDot);
+
+	return true;
 }
 
 void ShowError()
